1489cdf8ab6b4b26497db8f146d0ea7707fdb897
Letao WANG <letaowang7@gmail.com>
2022-01-30 00:59:39 +0100

    Modify mcts and still not work :(

--- src/mcts.py
+++ src/mcts.py
@@ -5,30 +5,31 @@ import numpy as np
 class Node(object):
     def __init__(self, state: State, parent=None):
         self.state = state
+        self._next_states = state.next_states
         self.value = 0
         self.number_visit = 0
         self.parent = parent
         self.children = []
 
     def __repr__(self):
-        return str(self.state.last_action) + ' \n' + str(self.state)
+        return 'value: ' + str(self.value) + ', numbe visit: ' + str(self.number_visit) + '\n'
 
     @property
     def n(self):
         return self.number_visit
 
-    @property
-    def next_nodes(self):
-        """
-
-        Returns: list of Node
-        """
-        return [Node(next_state, self) for next_state in self.state.next_states]
+    # @property
+    # def next_states(self):
+    #     """
+    #
+    #     Returns: list of State
+    #     """
+    #     if not hasattr(self, '_next_states'):
+    #         pass
+    #     return self.state.next_states
 
     def expand(self):
-        print('Before: ' + str(self.state.next_states))
-        next_state = self.state.next_states.pop()
-        print('After: ' + str(self.state.next_states))
+        next_state = self._next_states.pop()
         child_node = Node(next_state, self)
         self.children.append(child_node)
         return child_node
@@ -50,9 +51,12 @@ class Node(object):
             self.parent.backpropagation(result)
 
     def is_fully_expanded(self):
-        return len(self.next_nodes) == 0
+        return len(self._next_states) == 0
 
     def best_child(self, c_param=1.4):
+        if not self.children:
+            print('WRONG!')
+            return self
         choices_weights = [
             self.value + c_param * np.sqrt((2 * np.log(self.n) / c.n))
             for c in self.children
@@ -67,7 +71,7 @@ class Tree:
 
     def best_action(self, simulations_number):
         for _ in range(0, simulations_number):
-            # Selection and expansion
+            # Selection
             v = self.select()
             # Simulation
             result = v.simulate()
@@ -75,14 +79,14 @@ class Tree:
             v.backpropagation(result)
         return self.root.best_child(c_param=0)
 
-    def select(self):
-        current_node = self.root
-        while not current_node.is_terminal_node():
-
-            # Expansion
-            if not current_node.is_fully_expanded():
-                return current_node.expand()
-            else:
-                # Selection
-                current_node = current_node.best_child()
-        return current_node
+    # def select(self):
+    #     current_node = self.root
+    #     while not current_node.is_terminal_node():
+    #
+    #         # Expansion
+    #         if not current_node.is_fully_expanded():
+    #             return current_node.expand()
+    #         else:
+    #             # Selection
+    #             current_node = current_node.best_child()
+    #     return current_node
